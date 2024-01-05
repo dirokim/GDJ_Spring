65333a8caf3a3f035e04eecee8ae5da29a3c1bab
dirokim <dirokim7@gmail.com>
2024-01-05 13:41:33 +0900

    ad

--- src/test/java/com/winter/app/departments/DepartmentDAOTest.java
+++ src/test/java/com/winter/app/departments/DepartmentDAOTest.java
@@ -1,5 +1,6 @@
 package com.winter.app.departments;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -9,23 +10,53 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.winter.app.MyTest;
+import com.winter.app.departments.DepartmentDAO;
+import com.winter.app.departments.DepartmentDTO;
 
- public class DepartmentDAOTest extends MyTest {
-	 
+public class DepartmentDAOTest extends MyTest{
+	
 	@Autowired
 	private DepartmentDAO departmentDAO;
 	
-	@Test	
-	public void getListTest() throws Exception {
-		List<DepartmentDTO>ar = departmentDAO.getList();
-			assertNotEquals(0,ar.size());
-	}
-	@Test
-	public void getDetailTest() throws Exception {
+	public void deleteTest () throws Exception {
 		DepartmentDTO departmentDTO = new DepartmentDTO();
-		 departmentDTO.setDepartment_id(10);
-		 departmentDTO = departmentDAO.getDetail(departmentDTO);
+		departmentDTO.setDepartment_id(290);
+		int result = departmentDAO.delete(departmentDTO);
+		assertEquals(1, result);
+	}
+	
+	@Test
+	public void updateTest() throws Exception {
+		DepartmentDTO departmentDTO = new DepartmentDTO();
+		departmentDTO.setDepartment_id(280);
+		departmentDTO.setDepartment_name("Test");
+		departmentDTO.setManager_id(100);
+		departmentDTO.setLocation_id(1700);
+	int result = departmentDAO.update(departmentDTO);
+		assertEquals(1, result);
+	}
+	
+	@Test
+	public void addTest( ) throws Exception {
+		DepartmentDTO departmentDTO = new DepartmentDTO();
+		departmentDTO.setDepartment_name("Test");
+		departmentDTO.setManager_id(100);
+		departmentDTO.setLocation_id(1700);
+		int result = departmentDAO.add(departmentDTO);
+		assertEquals(1, result);
+	}
+	
+	@Test
+	public void getDetailTest( ) throws Exception{
+		DepartmentDTO departmentDTO = new DepartmentDTO();
+		departmentDTO.setDepartment_id(10);
+		departmentDTO = departmentDAO.getDetail(departmentDTO);
 		assertNotNull(departmentDTO);
-		
+	}
+	
+	@Test
+	public void getListTest() throws Exception {
+		List<DepartmentDTO> ar = departmentDAO.getList();
+		assertNotNull("list", ar.size());
 	}
 }
