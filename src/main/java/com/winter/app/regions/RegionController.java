008335255c37bb3f3e8ad93e61cb2f07e75cbad7
dirokim <dirokim7@gmail.com>
2024-01-05 13:37:38 +0900

    ad

--- src/main/java/com/winter/app/regions/RegionController.java
+++ src/main/java/com/winter/app/regions/RegionController.java
@@ -22,51 +22,37 @@ public class RegionController {
 
 	
 	
-//	@RequestMapping(value = "add", method = RequestMethod.POST)
-//	public String add(RegionDTO regionDTO, Model model) throws Exception {
-////		String id = request.getParameter("region_id");
-////		String name = request.getParameter("region_name");
-////		
-////		RegionDTO regionDTO = new RegionDTO();
-////		regionDTO.setRegion_id(Integer.parseInt(id));
-////		regionDTO.setRegion_name(name);
-//		
-//		int result = this.regionDAO.add(regionDTO);
-//		
-//		String msg="등록 실패";
-//		if(result>0) {
-//			msg = "등록 성공";
-//		}
-//		
-//		model.addAttribute("msg", msg);
-//		model.addAttribute("path", "./list");
-//		
-//		return "commons/result";
-//	}
+	@RequestMapping(value = "add", method = RequestMethod.POST)
+	public String add(RegionDTO regionDTO, Model model) throws Exception {
 	
-	@RequestMapping(value = "add", method = RequestMethod.GET)
-	public String add() {
 		
-		// /WEB-INF/views/   .jsp
-		return "regions/add";
+		int result = reigonService.add(regionDTO);
+		
+		String msg="등록 실패";
+		if(result>0) {
+			msg = "등록 성공";
+		}
+		
+		model.addAttribute("msg", msg);
+		model.addAttribute("path", "./list");
+		
+		return "commons/result";
 	}
+	
+
 	
 	@RequestMapping(value ="detail", method = RequestMethod.GET)
 	//파라미터의 이름과 타입을 동일하게 선언
 	//id
 	public String detail(Integer region_id, Model model)throws Exception{
 		
-		RegionDAO regionDAO = new RegionDAO();
+		
 		RegionDTO regionDTO = new RegionDTO();
 		//String id = request.getParameter("region_id");
-		
 		regionDTO.setRegion_id(region_id);
-		
-		regionDTO = regionDAO.getDetail(regionDTO);
-		
+		regionDTO = reigonService.getdetail(regionDTO);
 		//request.setAttribute("dto", regionDTO);
 		model.addAttribute("dto", regionDTO);
-		
 		return "regions/detail";
 	}
 	
@@ -76,10 +62,8 @@ public class RegionController {
 		System.out.println("Regions List");
 		RegionDAO regionDAO = new RegionDAO();
 		List<RegionDTO> ar = reigonService.getlist();
-		
 		mv.addObject("list", ar);
 		mv.setViewName("regions/list");
-		
 		//return "regions/list";
 		return mv;
 	}
--- src/main/java/com/winter/app/regions/RegionService.java
+++ src/main/java/com/winter/app/regions/RegionService.java
@@ -13,12 +13,30 @@ public class RegionService {
 	@Autowired
 	private RegionDAO regionDAO;
 	
+	
+	//delete
+	public int delete(RegionDTO regionDTO)throws Exception{
+		return regionDAO.delete(regionDTO);
+	}
+	
+	//update
+	public int update(RegionDTO regionDTO)throws Exception {
+		return regionDAO.update(regionDTO);
+	}
+	
+	//insert
+	public int add(RegionDTO regionDTO) throws Exception {
+		return regionDAO.add(regionDTO);
+		
+	}
+	
+	//detail
+	public RegionDTO getdetail(RegionDTO regionDTO)throws Exception {
+		return regionDAO.getDetail(regionDTO);
+	}
+	//list
 	public List<RegionDTO> getlist()throws Exception {
 		List<RegionDTO> ar = this.regionDAO.getList();
-		
-		
-		
-		
 		return ar;
 	}
 	
